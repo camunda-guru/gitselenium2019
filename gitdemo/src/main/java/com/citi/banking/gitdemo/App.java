4151699a50d1633c47c2a4a151e21ce9c5f8964e
Parameswaribala <Parameswaribala@gmail.com>
2019-04-12 01:24:30 +0530

    committing

---
+++ gitdemo/src/main/java/com/citi/banking/gitdemo/App.java
@@ -0,0 +1,13 @@
+package com.citi.banking.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
